5ec4d85530c89369b5ad481c8b5a15326dc9590f
BY1994 <lovelychoco100@gmail.com>
2019-01-03 10:25:25 +0900

    Edit browser.py

--- browser.py
+++ browser.py
@@ -1,14 +1,17 @@
 import webbrowser
 
 # 1. 사용자의 입력을 받아 검색하기
-# keyword = input("검색어를 입력해 주세요: ")
+keyword = input("검색어를 입력해 주세요: ")
+url = "https://search.daum.net/search?q="
+
+webrowser.open(url + keyword)
 
 # 모모랜드 모든 멤버들의 검색 페이지를 한 번에 여는 코드
 
-url = "https://search.daum.net/search?q="
 
-momo = ["나윤", "혜빈", "아인", "낸시", "주이", "연우", "제인", "데이지", "태하"]
 
-# momo라고 하는 리스트를 한 번씩 돌면서, 웹 브라우저를 연다.
-for keyword in momo:
-    webbrowser.open(url + keyword)
+# momo = ["나윤", "혜빈", "아인", "낸시", "주이", "연우", "제인", "데이지", "태하"]
+
+# # momo라고 하는 리스트를 한 번씩 돌면서, 웹 브라우저를 연다.
+# for keyword in momo:
+#     webbrowser.open(url + keyword)
